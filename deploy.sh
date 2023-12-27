a1fc843f2d23d47de5302b13e768e60cfd40f12d
evel <957010831@qq.com>
2023-12-27 15:14:03 +0800

    feat: deploy添加配置 添加deploy文件中配置

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,18 @@
 set -e
 
 # 构建
-npm run build
+# npm run build
 
 # 进入生成的构建文件夹
 cd dist
 
 # 如果你是要部署到自定义域名
 # echo 'www.example.com' > CNAME
+git config --global user.name "evel"
+git config --global user.email "957010831@qq.com"
 
-git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'feat: deploy'
 
 # 如果你想要部署到 https://<USERNAME>.github.io
 git push -f git@github.com:EvalGitHub/EvalGitHub.github.io.git master
